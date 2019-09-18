18b7da30e27fec2c2400697c21e4cd4377d44689
Roy N Emanuel II <roy.emanuel@gmail.com>
2019-09-17 20:31:47 -0400

    Started working through the chapter five stuff. Good stuff.

---
+++ chap6.r
@@ -0,0 +1,32 @@
+library(rethinking)
+library(tidyverse)
+data(WaffleDivorce)
+d <- WaffleDivorce
+
+## standardize predictor
+d <-
+    d %>%
+    mutate(MedianAgeMarriage.s = (MedianAgeMarriage - mean(MedianAgeMarriage))/
+               sd(MedianAgeMarriage))
+
+## Fit model
+m5.1 <-
+    map(
+        alist(
+            Divorce ~ dnorm(mu , sigma),
+            mu  <- a  + bA * MedianAgeMarriage.s,
+            a ~ dnorm(10, 10),
+            bA ~ dnorm(0, 1),
+            sigma ~ dunif(0, 10)),
+        data = d)
+
+
+## Compute percentile interval of mean
+MAM.seq <- seq(from = -3, to= 3.5, length.out=30)
+mu <- link(m5.1, data=data.frame(MedianAgeMarriage.s=MAM.seq))
+mu.PI <- apply(mu, 2, PI)
+
+## Plot it all
+plot( Divorce ~ MedianAgeMarriage.s, data=d, col=rangi2)
+abline(m5.1)
+shade(mu.PI, MAM.seq)
