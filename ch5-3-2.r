c6780e32cb51ba2592a114f43c1fa32973ca0513
Roy Emanuel <remanuel@alum.mit.edu>
2019-09-26 18:27:21 -0400

    some changes

--- ch5-3-2.r
+++ ch5-3-2.r
@@ -38,3 +38,19 @@ m5.12 <-
 precis(m5.12, digits=3)
 
 pairs( ~ kcal.per.g + perc.fat + perc.lactose, data = d, col=rangi2)
+cor(d$perc.fat, d$perc.lactose)
+
+sim.coll <- function(r=0.9){
+    rnorm(nrow(d), mean=r * d$perc.fat, sd = sqrt(1-r^2)*var(d$perc.fat))
+    m <- lm(kcal.per.g ~ perc.fat + x, data = d)
+    sqrt(diag(vcov(m)))[2]
+}
+
+rep.sim.coll <- function(r = 0.9, n = 100) {
+    stddev <- replicate(n, sim.coll(r))
+    mean(stddev)
+}
+
+r.seq <- seq(from=0, to= 0.99, by=0.01)
+stddev <- sapply(r.seq, function(z) rep.sim.coll(r=z, n=100))
+plot(stddev ~ r.seq, type="l", col=rangi2, lwd=2, xlab = "correlation")
