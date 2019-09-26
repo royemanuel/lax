e25119e13a17d62023a6570bd4806c6e8fb061bd
Roy Emanuel work <roy.emanuel@gmail.com>
2019-09-26 12:16:33 -0400

    Wrapped up Chapter 5. On to the homeworks.

--- ch5-4.r
+++ ch5-4.r
@@ -66,4 +66,29 @@ mu.S <- post$a + post$bS
 
 precis(data.frame(mu.ape, mu.NWM, mu.OWM, mu.S))
 
+diff.NWM.OWM <- mu.NWM - mu.OWM
+quantile(diff.NWM.OWM, probs=c(0.025,0.5,0.975))
 
+m5.17 <-
+    map(
+        alist(
+            kcal.per.g ~ dnorm(mu, sigma),
+            mu <- a + bNWM * clade.NWM + bOWM * clade.OWM + bS * clade.S + bF * perc.fat,
+            a ~ dnorm(0.6, 10),
+            bF ~ dnorm(0,1),
+            bNWM ~ dnorm(0,1),
+            bOWM ~ dnorm(0,1),
+            bS ~ dnorm(0,1),
+            sigma ~ dunif(0, 10)),
+        data = d)
+
+(d$clade_id <- coerce_index(d$clade))
+
+m5.16alt <-
+    map(
+        alist(
+            kcal.per.g ~ dnorm(mu, sigma),
+            mu <- a[clade_id],
+            a[clade_id] ~ dnorm(0.6, 10),
+            sigma ~ dunif(0, 10)),
+        data = d)
