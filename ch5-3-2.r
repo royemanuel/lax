d46cdec55659502439341b31fe70bddf6aad3c54
Roy Emanuel work <roy.emanuel@gmail.com>
2019-09-26 10:32:15 -0400

    Working through section 5.4

--- ch5-3-2.r
+++ ch5-3-2.r
@@ -38,3 +38,42 @@ m5.12 <-
 precis(m5.12, digits=3)
 
 pairs( ~ kcal.per.g + perc.fat + perc.lactose, data = d, col=rangi2)
+
+## Post-treatment bias work
+
+## number of plants
+N <- 100
+
+## simulate initial hieghts
+h0 <- rnorm(N,10,2)
+
+## assign treatments and simulte fungus and growth
+treatment <- rep(0:1, each = N/2)
+fungus  <- rbinom(N, size=1, prob=0.5 - treatment * 0.4)
+h1 <- h0 + rnorm(N, 5 - 3 * fungus)
+
+## data frame
+d <- data.frame(h0=h0, h1=h1, treatment=treatment, fungus=fungus)
+
+m5.13 <-
+    map(
+        alist(
+            h1 ~ dnorm(mu, sigma),
+            mu  <- a + bH * h0 + bT * treatment + bF * fungus,
+            a ~ dnorm(0, 100),
+            bT ~ dnorm(0, 10),
+            bF ~ dnorm(0, 10),
+            bH ~ dnorm(0, 10),
+            sigma ~ dunif(0, 10)),
+        data=d)
+
+m5.14 <-
+    map(
+        alist(
+            h1 ~ dnorm(mu, sigma),
+            mu  <- a + bH * h0 + bT * treatment,
+            a ~ dnorm(0, 100),
+            bT ~ dnorm(0, 10),
+            bH ~ dnorm(0, 10),
+            sigma ~ dunif(0, 10)),
+        data=d)
---
+++ ch5-4.r
@@ -0,0 +1,69 @@
+library(tidyverse)
+library(rethinking)
+
+data(Howell1)
+d <- Howell1
+str(d)
+
+m5.15 <-
+    map(
+        alist(
+            height ~ dnorm(mu, sigma),
+            mu  <- a + bM * male,
+            a ~ dnorm(178, 100),
+            bM ~ dnorm(0, 10),
+            sigma ~ dunif(0,50)),
+        data=d)
+precis(m5.15)
+
+post <- extract.samples(m5.15)
+mu.male <- post$a + post$bM
+PI(mu.male)
+
+## Reparamaterize the model to include both male and female
+
+m5.15b <-
+    map(
+        alist(
+            height ~ dnorm(mu, sigma),
+            mu <- af * (1-male) + am * male,
+            af ~ dnorm(178, 100),
+            am ~ dnorm(178, 100),
+            sigma ~ dnorm(0,10)),
+        data = d)
+precis(m5.15b)
+
+data(milk)
+d <- milk
+unique(d$clade)
+
+(d$clade.NWM <- ifelse(d$clade=="New World Monkey", 1, 0))
+(d$clade.OWM <- ifelse(d$clade=="Old World Monkey", 1, 0))
+(d$clade.S <- ifelse(d$clade=="Strepsirrhine", 1, 0))
+
+m5.16 <-
+    map(
+        alist(
+            kcal.per.g ~ dnorm(mu, sigma),
+            mu <- a + bNWM * clade.NWM + bOWM * clade.OWM + bS * clade.S,
+            a ~ dnorm(0.6, 10),
+            bNWM ~ dnorm(0,1),
+            bOWM ~ dnorm(0,1),
+            bS ~ dnorm(0,1),
+            sigma ~ dunif(0, 10)),
+        data = d)
+
+precis(m5.16)
+
+## sample the posterior
+post <- extract.samples(m5.16)
+
+## compute averages for every category
+mu.ape <- post$a
+mu.NWM  <- post$a + post$bNWM
+mu.OWM <- post$a + post$bOWM
+mu.S <- post$a + post$bS
+
+precis(data.frame(mu.ape, mu.NWM, mu.OWM, mu.S))
+
+
