1d6abf49a93a38bdeca00e18c65ad8bc026b9a0a
Roy Emanuel <remanuel@alum.mit.edu>
2019-09-29 20:12:07 -0400

    Added rvest and my first data scraping from the web!!!

--- ch5hwrk.r
+++ ch5hwrk.r
@@ -42,3 +42,38 @@ mh3 <-
             b2 ~ dnorm(1,10),
             sigma ~ dunif(0,50)),
         data = d)
+
+## Divorce rate may cause a higher marriage rate because more couples
+## are available if they are divorcing. Individuals can have more than
+## wone marriage which is more
+
+## Marriage rate ~ norm(mu, sigma)
+## mu <- a + bDR * divorce.rate + bMA * median.age
+
+library(XML)
+mormon <- "https://www.worldatlas.com/articles/mormon-population-by-state.html"
+
+mormon.tb <- readHTMLTable(mormon, header=T, which=1, stringsAsFactors=FALSE)
+library(rvest)
+
+mm <-read_html(mormon)  %>%
+    html_node("#artReg-table :nth-child(1)") %>%
+    html_table(.) %>%
+    mutate(EMP = as.numeric(gsub(",", "", `Estimated Mormon Population`)),
+           Location = `<U+FEFF>State`)
+
+
+data(WaffleDivorce)
+d <- WaffleDivorce
+
+## standardize predictor
+d <-
+    d %>%
+    mutate(MedianAgeMarriage.s = (MedianAgeMarriage - mean(MedianAgeMarriage))/
+               sd(MedianAgeMarriage))
+
+head(d)
+
+d <-
+    left_join(x = d, y = mm, by = "Location") %>%
+    mutate(mormon.std = (EMP - mean(EMP)) / sd(EMP))
