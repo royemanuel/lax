6c4b38eb0be1773509589084a6fd72aedd2f0408
Roy Emanuel <remanuel@alum.mit.edu>
2019-10-14 06:41:11 -0400

    DOing some good work with INformation criteria

--- ch6.r
+++ ch6.r
@@ -12,11 +12,11 @@ m6.1 <- lm( brain ~ mass, data = d)
 1 - var(resid(m6.1))/var(d$brain)
 m6.2 <- lm( brain ~ mass + I(mass^2), data=d)
 m6.3 <- lm( brain ~ mass + I(mass^2) + I(mass^3), data=d)
-m6.4 <- lm( brain ~ mass + I(mass^2) + I(mass^3)
-         I(mass^4), data=d)
-m6.5 <- lm( brain ~ mass + I(mass^2) + I(mass^3)
+m6.4 <- lm( brain ~ mass + I(mass^2) + I(mass^3) +
+         I(mass^4), data=d) 
+m6.5 <- lm( brain ~ mass + I(mass^2) + I(mass^3) +
          I(mass^4) + I(mass^5), data=d)
-m6.6 <- lm( brain ~ mass + I(mass^2) + I(mass^3)
+m6.6 <- lm( brain ~ mass + I(mass^2) + I(mass^3) +
          I(mass^4) + I(mass^5) + I(mass^6), data=d)
 
 
@@ -36,5 +36,43 @@ dev <- (-2) * sum(dnorm(d$brain, mean=theta[1] + theta[2] * d$mass.s,
 
 
 
+data(milk)
+d <- milk[complete.cases(milk),]
+d$neocortex <- d$neocortex.perc / 100
+dim(d)
 
+a.start <- mean(d$kcal.per.g)
+sigma.start <- log(sd(d$kcal.per.g))
+m6.11 <-
+    map(
+        alist(
+            kcal.per.g ~ dnorm(a, exp(log.sigma))),
+        data = d,
+        start = list(a=a.start, log.sigma=sigma.start))
 
+m6.12 <-
+    map(
+        alist(
+            kcal.per.g ~ dnorm(mu, exp(log.sigma)),
+            mu <- a + bn * neocortex),
+        data = d,
+        start = list(a=a.start, bn=0, log.sigma=sigma.start))
+
+m6.13 <-
+    map(
+        alist(
+            kcal.per.g ~ dnorm(mu, exp(log.sigma)),
+            mu <- a + bm * log(mass)),
+        data = d,
+        start = list(a=a.start, bm = 0, log.sigma=sigma.start))
+
+m6.14 <-
+    map(
+        alist(
+            kcal.per.g ~ dnorm(mu, exp(log.sigma)),
+            mu <- a + bm * log(mass) + bn * neocortex),
+        data = d,
+        start = list(a=a.start, bm = 0, bn = 0, log.sigma=sigma.start))
+            
+            
+                  
