7b8f4e7cd0efdc078f294001f8dbb91489d7c894
Roy Emanuel <remanuel@alum.mit.edu>
2019-10-19 09:31:27 -0400

    Centering demonstration

--- ch7contint.r
+++ ch7contint.r
@@ -33,3 +33,37 @@ m7.7 <-
 precis(m7.7)
 coeftab(m7.6, m7.7)
 compare(m7.6, m7.7)
+
+## centering the predictor variables
+d$shade.c <- d$shade - mean(d$shade)
+d$water.c <- d$water - mean(d$water)
+
+m7.8 <-
+    map(
+        alist(
+            blooms ~ dnorm(mu, sigma),
+            mu <- a + bW * water.c + bS * shade.c,
+            a ~ dnorm(130 ,100),
+            bW ~ dnorm( 0, 100),
+            bS ~ dnorm(0, 100),
+            sigma ~ dunif(0, 100)),
+        data = d,
+        start=list(a=mean(d$blooms), bW=0, bW=0, sigma=sd(d$blooms)))
+precis(m7.8)
+
+m7.9 <-
+    map(
+        alist(
+            blooms ~ dnorm(mu, sigma),
+            mu <- a + bW * water.c + bS * shade.c + bWS * shade.c * water.c,
+            a ~ dnorm(130,100),
+            bW ~ dnorm( 0, 100),
+            bS ~ dnorm(0, 100),
+            bWS ~ dnorm(0,100),
+            sigma ~ dunif(0, 100)),
+        data = d,
+        start=list(a=mean(d$blooms), bW=0, bW=0, sigma=sd(d$blooms)))
+
+precis(m7.9)
+coeftab(m7.8, m7.9)
+compare(m7.8, m7.9)
