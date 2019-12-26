e703fefa835039b78140cf85bf0bdac89dcc37e5
Roy Emanuel <remanuel@alum.mit.edu>
2019-12-26 14:38:48 -0500

    Added a new JAGS model to play with

--- exampleJAGS.r
+++ exampleJAGS.r
@@ -18,9 +18,17 @@ listdata <-
 
 mod <- "model {
     for (i in 1:Ntotal){
-        y[i] ~ dbinom(p, numTrial[i])
+        y[i] ~ dbinom(p[i], numTrial[i])
+        p[i] ~ dbeta(2, 5)
     }
-    p ~ dbeta(2, 5)
 }"
 
-writeLines
+write(mod, "mod.txt")
+model <- jags.model("mod.txt",
+                    data=listdata,
+                    n.chains = 2)
+update(model, n.iter=1000)
+
+samples <- coda.samples(model,
+                        variable.names = c("p"),
+                        n.iter=1000)
