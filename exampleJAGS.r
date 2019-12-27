007daa5760c39ee4b092bfb2100e72afd849af5b
Roy Emanuel <remanuel@alum.mit.edu>
2019-12-26 19:21:11 -0500

    Working JAGS model with cell means whatever that means.

--- exampleJAGS.r
+++ exampleJAGS.r
@@ -5,30 +5,49 @@ library("tidyverse")
 ## Build the fake data
 
 ## Number of trials
-numTrial <- floor(rbeta(200, 2,2) * 20)
-Ntotal  <- 200
+numTrial <- floor(rbeta(400, 2,2) * 20)
+Ntotal  <- 400
 
-y <- rbinom(n = Ntotal,size = numTrial, p = 0.25)
+y <- c(rbinom(n = Ntotal/2,size = numTrial[1:200], p = 0.35),
+       rbinom(n = Ntotal/2,size = numTrial[201:400], p = 0.15))
+
+t <- c(rep(2, 200), rep(1, 200))
+
+Nlvl <- length(unique(t))
 
 listdata <-
     list(
         y = y,
         numTrial = numTrial,
-        Ntotal = Ntotal)
+        t = t,
+        Ntotal = Ntotal,
+        Nlvl = Nlvl)
 
 mod <- "model {
     for (i in 1:Ntotal){
         y[i] ~ dbinom(p[i], numTrial[i])
-        p[i] ~ dbeta(2, 5)
+        p[i] ~ dbeta(omega[t[i]] * (kappa - 2) + 1, (1 - omega[t[i]]) * (kappa-2) + 1)
     }
+    for (j in 1:Nlvl){
+        omega[j] <- ilogit(a0 + a[j])
+        a[j] ~ dnorm(0, 1/aSigma^2)
+    }
+    a0 ~ dnorm(0, 1/4)
+    aSigma ~ dgamma(1.64, .32)
+    kappa <- kappaMinusTwo + 2
+    kappaMinusTwo ~ dgamma( 0.01 , 0.01 )  # mean=1 , sd=10 (generic vague)
+    # Convert a0,a[] to sum-to-zero b0,b[] :
+    for ( j in 1:Nlvl ) { m[j] <- a0 + a[j] } # cell means 
+    b0 <- mean( m[1:Nlvl] )
+    for ( j in 1:Nlvl ) { b[j] <- m[j] - b0 }
 }"
 
-write(mod, "mod.txt")
+writeLines(mod, "mod.txt")
 model <- jags.model("mod.txt",
                     data=listdata,
-                    n.chains = 2)
+                    n.chains = 4)
 update(model, n.iter=1000)
 
 samples <- coda.samples(model,
-                        variable.names = c("p"),
-                        n.iter=1000)
+                        variable.names = c("a0", "a", "b0", "b"),
+                        n.iter=5000)
