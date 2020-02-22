05cb7d286f4f35533c0d50e6dfed5c5848a58268
Roy Emanuel <remanuel@alum.mit.edu>
2020-02-22 13:38:12 -0500

    Two block model

--- ch12-3.r
+++ ch12-3.r
@@ -23,6 +23,44 @@ m12.4 <-
     )
 
 post <- extract.samples(m12.4)
+
+## This is the magic function that builds the real means that are useful
 total_a_actor <- sapply(1:7, function(actor) post$a + post$a_actor[,actor])
 round(apply(total_a_actor, 2, mean), 2)
+
+## Two types of clusters
+
+d$block_id  <- d$block
+
+m12.5 <-
+    map2stan(
+        alist(
+            pulled_left ~ dbinom(1, p),
+            logit(p) <- a + a_actor[actor] +
+                a_block[block_id] +
+                (bp + bpc * condition) * prosoc_left,
+            a_actor[actor] ~ dnorm(0, sigma_actor),
+            a_block[block_id] ~ dnorm(0, sigma_block),
+            c(a, bp, bpc) ~ dnorm(0,10),
+            sigma_actor ~ dcauchy(0,1),
+            sigma_block ~ dcauchy(0,1)),
+        data = d, warmup = 1000, iter = 6000, chains = 4, cores = 3
+    )
+
+precis(m12.5, depth = 2)
+plot(precis(m12.5, depth =2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
