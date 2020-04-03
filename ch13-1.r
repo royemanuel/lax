08c5e0eebd88bf0fb0df2d26637536ab6a4ae65b
Roy N Emanuel II <roy.emanuel@gmail.com>
2020-04-03 14:47:03 -0400

    13.1 complete

--- ch13-1.r
+++ ch13-1.r
@@ -54,3 +54,45 @@ m13.1 <-
 
 post <- extract.samples(m13.1)
 dens(post$R[,1,2])
+
+
+## compute unpooled estimates directly from the data
+a1 <- sapply(1:N_cafes,
+             function(i) mean(wait[cafe_id == i & afternoon == 0]))
+b1 <- sapply(1:N_cafes,
+             function(i) mean(wait[cafe_id == i & afternoon == 1])) - a1
+
+## extract posterior means of partially pooled estimates
+post <- extract.samples(m13.1)
+a2 <- apply(post$a_cafe, 2, mean)
+b2 <- apply(post$b_cafe, 2, mean)
+
+## plot both and connect with lines
+plot(a1, b1, xlab = "intercept", ylab = "slope",
+     pch = 16, col = rangi2, ylim = c(min(b1) - 0.1, max(b1) + 0.1),
+     xlim = c(min(a1) - 0.1, max(a1) + 0.1))
+points(a2, b2, pch = 1)
+for (i in 1:N_cafes) lines(c(a1[i], a2[i]), c(b1[i], b2[i]))
+
+## and to superimpose the contours of the population
+## compute posterior mean bivariate Gaussian
+
+Mu_est <- c(mean(post$a), mean(post$b))
+rho_est <- mean(post$R[,1,2])
+sa_est <- mean(post$sigma_cafe[,1])
+sb_est <- mean(post$sigma_cafe[,2])
+cov_ab <- sa_est * sb_est * rho_est
+Sigma_est  <- matrix(c(sa_est^2, cov_ab, cov_ab, sb_est^2), ncol = 2)
+
+## draw contours
+library(ellipse)
+for (l in c(0.1, 0.3, 0.5, 0.8, 0.99)) {
+    lines(ellipse(Sigma_est, centre = Mu_est, level = l),
+          col = col.alpha("black", 0.2))
+}
+
+## convert varying effects to waiting times
+wait_morning <- a1
+wait_afternoon_1 <- a1  + b1
+wait_morning_2 <- a2
+wait_morning2 <- a2  + b2
