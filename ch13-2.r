bb666ec896d41791375ae7f69206703ce35b8c94
Emanuel, Roy N <roy.emanuel@jhuapl.edu>
2020-04-04 15:59:54 -0400

    13.2 complete

--- ch13-2.r
+++ ch13-2.r
@@ -17,3 +17,63 @@ m13.2 <-
             bm ~ dnorm(0, 1),
             sigma_dept ~ dcauchy(0, 2)),
         data = d, warmup = 500, iter = 4500, chains = 3)
+precis(m13.2, depth = 2)
+
+m13.3 <-
+    map2stan(
+        alist(
+            admit ~ dbinom(applications, p),
+            logit(p) <- a_dept[dept_id] + bm_dept[dept_id] * male,
+            c(a_dept, bm_dept)[dept_id] ~ dmvnorm2(c(a, bm), sigma_dept, Rho),
+            a ~ dnorm(0, 10),
+            bm ~ dnorm(0,1),
+            sigma_dept ~ dcauchy(0, 2),
+            Rho ~ dlkjcorr(2)),
+        data = d, warmup = 1000, iter = 5000, chains = 4, cores = 3)
+
+
+
+## Tried to redo the plot from 13.1, but this isn't working so well.
+## d <-
+##     d %>%
+##     mutate(acc_rate = admit / applications)
+
+## a1 <-
+##     d %>%
+##     group_by(dept) %>%
+##     summarise(admit = sum(admit), app = sum(applications)) %>%
+##     transmute(acc_rate = admit / app) 
+## b1  <- d %>% filter(male == 1) %>% select(acc_rate)
+## b1l <- log((b1-a1)/(1-(b1-a1)))
+
+## a1l <- log((a1/(1-a1)))
+
+## ## extract sample means from partially pooled estimates
+## post <- extract.samples(m13.3)
+## a2 <- apply(post$a_dept, 2, mean)
+## pa2 <- exp(a2) / (1 + exp(a2))
+## b2 <- apply(post$bm_dept, 2, mean)
+## pb2 <- exp(a2 + b2) / (1 + exp(a2 + b2)) - pa2
+## ## Plot each and connect hem
+## plot(a1$acc_rate, b1$acc_rate, xlab = "intercept", ylab = "slope",
+##      pch = 16, col = rangi2 , ylim= c(min(b1)-0.1, max(b1) + 0.1),
+##      xlim = c(min(a1) - 0.1, max(a1) + 0.1))
+
+## points(pa2, pb2, pch = 1)
+## for( i in 1:6) lines(c(a1$acc_rate[i], pa2[i]), c(b1$acc_rate[i], pb2[i]))
+
+
+## library(ellipse)
+
+m13.4 <-
+    map2stan(
+        alist(
+            admit ~ dbinom( applications, p),
+            logit(p) <- a_dept[dept_id],
+            a_dept[dept_id] ~ dnorm( a, sigma_dept),
+            a ~ dnorm(0, 10),
+            sigma_dept ~ dcauchy(0,2)
+        ),
+        data = d, warmup = 500, iter = 4500, chains = 3)
+
+compare(m13.2, m13.3, m13.4)
